0c9f4e4a14aed461e74309b8c9d2fe034b9bb109
regaron <rabigiri18@gmail.com>
2018-08-17 11:44:52 +0545

    Exec

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
-
+const openurl = require("openurl")
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -22,9 +22,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+openurl.open("http://localhost:8080");
 module.exports = app;
